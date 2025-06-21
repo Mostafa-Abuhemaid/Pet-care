92d3ae0f65ce1f67ef939208b9994045ff4009b3
Mostafa-Abuhemaid <mostafaabohameed186@gmail.com>
2025-06-21 18:10:52 +0300

    Add Project

---
+++ scr/Web.APIs/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Application.Interfaces;
+
+namespace Web.APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("Lock")]
+        public async Task<IActionResult> LockUser(string email)
+        {
+            var user = await _userService.LockUserByEmailAsync(email);
+            return user.Success ? Ok(user) : BadRequest(user);
+        }
+
+
+        //  [Authorize(Roles = "Admin")]
+        [HttpPost("Unlock")]
+        public async Task<IActionResult> UnlockUser(string email)
+        {
+            var user = await _userService.UnlockUserByEmailAsync(email);
+            return user.Success ? Ok(user) : BadRequest(user);
+        }
+        // [Authorize(Roles = "Admin")] 
+        [HttpDelete("DeleteAccountByEmail")]
+        public async Task<IActionResult> DeleteUserByEmail(string email)
+        {
+            var user = await _userService.DeleteUserByEmailAsync(email);
+            return user.Success ? Ok(user) : BadRequest(user);
+        }
+        // [Authorize(Roles = "Admin")] 
+        [HttpGet("GetAllUsers")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var user = await _userService.GetAllUsersAsync();
+            return user.Success ? Ok(user) : BadRequest(user);
+        }
+    }
+}
---
+++ scr/Web.Domain/Entites/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Domain.Enums;
+
+namespace Web.Domain.Entites
+{
+    public class AppUser : IdentityUser
+    {
+   public Gender gender {  get; set; }
+    }
+}
