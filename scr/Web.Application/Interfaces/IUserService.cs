62698e040d6de8c0cad91fb469f021ac716b22be
Mostafa-Abuhemaid <mostafaabohameed186@gmail.com>
2025-06-21 18:10:52 +0300

    Add Project

---
+++ scr/Web.Application/Interfaces/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Application.DTOs.UserDTO;
+using Web.Application.Response;
+
+namespace Web.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<BaseResponse<UserDto>> GetUserDetailsAsync(string userId);
+        Task<BaseResponse<List<UserDto>>> GetAllUsersAsync();
+        Task<BaseResponse<bool>> EditUserAsync( UserDto model);
+
+        Task<BaseResponse<bool>> LockUserByEmailAsync(string email);
+        Task<BaseResponse<bool>> UnlockUserByEmailAsync(string email);
+        Task<BaseResponse<bool>> DeleteUserByEmailAsync(string email);
+    }
+}
