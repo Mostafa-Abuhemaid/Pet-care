36606c3109b5e14c1221cc15c82a184aece20dd5
Mohamed Ahmed Salah Ibrahim <44170867+mohmedahmedsalah@users.noreply.github.com>
2025-06-26 05:47:20 +0300

    add database intro

---
+++ Web.Domain/Entites/VetClinic.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Web.Domain.Entites;
+
+namespace PetCare.Api.Entities
+{
+    public class VetClinic: BaseModel
+    {
+            [Required]
+            [MaxLength(100)]
+            public string Name { get; set; }
+
+            [Required]
+            [MaxLength(100)]
+            public string Specialty { get; set; }
+
+            [Required]
+            [MaxLength(200)]
+            public string Location { get; set; }
+
+            [Required]
+            public bool IsEmergencyAvailable { get; set; }
+
+            public ICollection<VetReview> Reviews { get; set; }
+    }
+}
+
+
---
+++ Web.Domain/Enums/Trainability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Web.Domain.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Trainability
+    {
+        Low = 0,
+        Moderate = 1,
+        High = 2,
+        Very_High = 3,
+    }
+}
