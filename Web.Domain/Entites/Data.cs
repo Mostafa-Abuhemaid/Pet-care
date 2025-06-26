246bffeaf02556e21ef5345aa853976e42908135
Mohamed Ahmed Salah Ibrahim <44170867+mohmedahmedsalah@users.noreply.github.com>
2025-06-26 05:47:20 +0300

    add database intro

---
+++ Web.Domain/Entites/Data.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Intrinsics.X86;
+using Web.Domain.Entites;
+using Web.Domain.Enums;
+using static Azure.Core.HttpHeader;
+
+namespace PetCare.Api.Entities
+{
+    public abstract class Data : BaseModel
+    {
+        
+
+
+
+    }
+
+}
