01b3327dc677cc021b09cb31b6dfecc4eeb34a69
Ahmed Momtaz Mohamed <am7883@fayoum.edu.eg>
2025-06-29 06:35:39 +0300

    Conver Service to Generic Service and Generic Controller and add Extension  Method(UserExtensions)to getuserID

---
+++ Web.Infrastructure/Migrations/20250629012622_testDatabase.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class testDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "FemaleAverageSize",
+                table: "Cat_Data",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "FemaleAverageWeight",
+                table: "Cat_Data",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FemaleTemperament",
+                table: "Cat_Data",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "MaleAverageSize",
+                table: "Cat_Data",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "MaleAverageWeight",
+                table: "Cat_Data",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MaleTemperament",
+                table: "Cat_Data",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FemaleAverageSize",
+                table: "Cat_Data");
+
+            migrationBuilder.DropColumn(
+                name: "FemaleAverageWeight",
+                table: "Cat_Data");
+
+            migrationBuilder.DropColumn(
+                name: "FemaleTemperament",
+                table: "Cat_Data");
+
+            migrationBuilder.DropColumn(
+                name: "MaleAverageSize",
+                table: "Cat_Data");
+
+            migrationBuilder.DropColumn(
+                name: "MaleAverageWeight",
+                table: "Cat_Data");
+
+            migrationBuilder.DropColumn(
+                name: "MaleTemperament",
+                table: "Cat_Data");
+        }
+    }
+}
