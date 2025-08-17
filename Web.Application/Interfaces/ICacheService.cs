14411506125822906f6652848c480cf58bf05c45
Ahmed Momtaz Mohamed <am7883@fayoum.edu.eg>
2025-08-17 16:38:55 +0300

    Add Distributed Cache

---
+++ Web.Application/Interfaces/ICacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Infrastructure.Service
+{
+    public interface ICacheService
+    {
+        Task<T?> GetAsync<T>(string key)where T:class;
+        Task SetAsync<T>(string key,T value)where T:class;
+        Task RemoveAsync(string key);
+    }
+}
